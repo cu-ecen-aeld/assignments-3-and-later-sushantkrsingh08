0398972a100f0ddba1394135a1e17b5c325b3ba6
Sushant Singh <ssingh332@gatech.edu>
2024-08-15 14:29:13 -0400

    make and compile linux

---
+++ finder-app/buildroot-config.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Script outline to install and build kernel.
+# Author: Siddhant Jajoo.
+
+set -e
+set -u
+
+OUTDIR=/tmp/aeld
+KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.1.10
+BUSYBOX_VERSION=1_33_1
+FINDER_APP_DIR=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=/home/ubuntu/linux_embedded/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+
+
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
+else
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
+fi
+
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
+fi
+
+# TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
+
+else
+    cd busybox
+fi
+
+# TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+
+cd ${OUTDIR}/rootfs
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a /bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a /bin/busybox | grep "Shared library"
+
+# TODO: Add library dependencies to rootfs
+cp -f ${TOOL_CHAIN_DIR}/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp -f ${TOOL_CHAIN_DIR}/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+cp -f ${TOOL_CHAIN_DIR}/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp -f ${TOOL_CHAIN_DIR}/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+
+# TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
+
+# TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} all
+
+# TODO: Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
+mkdir -p  ${OUTDIR}/rootfs/home/conf
+cp -f conf/username.txt      ${OUTDIR}/rootfs/home/conf
+cp -f conf/assignment.txt    ${OUTDIR}/rootfs/home/conf
+
+cp -f finder-test.sh    ${OUTDIR}/rootfs/home
+cp -f finder.sh         ${OUTDIR}/rootfs/home
+cp -f writer            ${OUTDIR}/rootfs/home
+cp -f autorun-qemu.sh   ${OUTDIR}/rootfs/home
+
+# TODO: Chown the root directory
+sudo chown root:root ${OUTDIR}/rootfs
+
+# TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root >${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
